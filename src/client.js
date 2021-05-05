370c02fb9214c4de58cc426970e01cd7335a9b4f
rudietuesdays <rae@throneless.tech>
2021-05-05 16:04:55 -0400

    update naming for saning proj id env var

--- src/client.js
+++ src/client.js
@@ -1,7 +1,7 @@
 import sanityClient from "@sanity/client";
 
 export default sanityClient({
-  projectId: process.env.NEXT_PUBLIC_SANITY_API_PROJECT_ID,
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
   apiVersion: "2021-05-04",
   dataset: "production",
   // authenticated requests can't be cached so we have to set useCdn to false
